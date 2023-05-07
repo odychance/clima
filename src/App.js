e201220ac9dc31266f3656888c9e7eba6fe0ac9c
odychance <odychance96@gmail.com>
2023-05-07 13:59:00 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment, useState, useEffect} from "react";
+import Header from "./components/Header";
+import Formulario from "./components/Formulario";
+import Clima from "./components/Clima";
+import Error from "./components/Error";
 
 function App() {
+
+  const [busqueda, setBusqueda] = useState({
+    ciudad: '',
+    pais: ''
+  })
+
+  const [consultar, setConsultar] = useState(false)
+
+  const [error, setError] = useState(false)
+
+  const [resultado, setResultado] = useState({})
+
+  const {ciudad, pais} = busqueda
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+
+      if(consultar) {
+        const appid = '380385ea1787f85b579d80272dc91a4c'
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appid}`
+  
+        const respuesta = await fetch(url)
+        const resultado = await respuesta.json()
+        setResultado(resultado)
+        setConsultar(false)
+
+        if(resultado.cod === '404') {
+          setError(true)
+        } else {
+          setError(false)
+        }
+      }
+        
+    }
+    consultarAPI()
+  }, [consultar])
+
+  let componente;
+  if(error) {
+    componente = <Error
+                  mensaje="No hay resultados" />
+  } else {
+    componente = <Clima 
+                  resultado={resultado} />
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    <Fragment>
+      <Header
+        titulo={"Clima React App"}
+      />
+
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col m6 s12">
+              <Formulario
+                busqueda={busqueda}
+                setBusqueda={setBusqueda}
+                setConsultar={setConsultar}
+              />
+            </div>
+            <div className="col m6 s12">
+                {componente}
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+
+    );
 }
 
 export default App;
